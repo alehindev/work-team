fd0cfa5a23288eabaa19846c6cd4d6f13ea30bb6
AlehinDev <71827715+alehindev@users.noreply.github.com>
2021-04-04 22:01:02 +0300

    update offline course

---
+++ mmmf-camp/js/olympiads.js
@@ -0,0 +1,37 @@
+ShowETournament();
+
+$("#btn-1").click(function(){
+    if($("#btn-1").hasClass("button_clicked") == false){
+        $("#btn-1").addClass("button_clicked");
+        $("#btn-2").removeClass("button_clicked");
+        $("#btn-3").removeClass("button_clicked");
+        $(".content__title").text("турнир европы");
+        ShowETournament();
+}});
+
+$("#btn-2").click(function(){
+        if($("#btn-2").hasClass("button_clicked") == false){
+            $("#btn-1").removeClass("button_clicked");
+            $("#btn-2").addClass("button_clicked");
+            $("#btn-3").removeClass("button_clicked");
+            $(".content__title").text("Кубок Математического Тигра");
+            ShowTiger();
+}});
+
+$("#btn-3").click(function(){
+        if($("#btn-3").hasClass("button_clicked") == false){
+            $("#btn-1").removeClass("button_clicked");
+            $("#btn-2").removeClass("button_clicked");
+            $("#btn-3").addClass("button_clicked");
+            $(".content__title").text("Олимпиада «Умосфера»");
+}});
+
+function ShowETournament(){
+    $(".etournament").show();
+    $(".tiger").hide();
+}
+
+function ShowTiger(){
+    $(".etournament").hide();
+    $(".tiger").show();
+}
