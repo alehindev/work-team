a5f28b21c4fe15eb1180bd2928cb1f2ac8e2810f
AlehinDev <71827715+alehindev@users.noreply.github.com>
2021-06-06 22:01:20 +0300

    updated menu

--- hnoco/js/navigation-container.js
+++ hnoco/js/navigation-container.js
@@ -1,14 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let elem = document.getElementById("nav__btn");
-    elem.addEventListener('click', function (e) {
+    let root = document.getElementsByClassName("navigation__btn")[0];
+
+    root.addEventListener('click', function (e) {
 
         if (document.getElementsByClassName("navigation__container-links")[0].classList.contains("navigation__container-unactive")) {
             document.getElementsByClassName("navigation__container-links")[0].classList.remove("navigation__container-unactive");
+
         } else {
             document.getElementsByClassName("navigation__container-links")[0].classList.add("navigation__container-unactive");
         }
 
     });
+    let root1 = document.getElementsByClassName("navigation__btn")[1];
 
+    root1.addEventListener('click', function (e) {
+
+        if (document.getElementsByClassName("navigation__container-links")[1].classList.contains("navigation__container-unactive")) {
+            document.getElementsByClassName("navigation__container-links")[1].classList.remove("navigation__container-unactive");
+            document.getElementsByClassName("navigation__container-links")[2].classList.add("navigation__container-unactive");
+
+        } else {
+            document.getElementsByClassName("navigation__container-links")[1].classList.add("navigation__container-unactive");
+        }
+
+    });
+    let root2 = document.getElementsByClassName("navigation__btn")[2];
+
+    root2.addEventListener('click', function (e) {
+
+        if (document.getElementsByClassName("navigation__container-links")[2].classList.contains("navigation__container-unactive")) {
+            document.getElementsByClassName("navigation__container-links")[2].classList.remove("navigation__container-unactive");
+            document.getElementsByClassName("navigation__container-links")[1].classList.add("navigation__container-unactive");
+
+        } else {
+            document.getElementsByClassName("navigation__container-links")[2].classList.add("navigation__container-unactive");
+        }
+
+    });
 
 });
